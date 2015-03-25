87b2c7f94d45c384f7b0e49641a5ca8b3e9f8a1f
morgl <morganl@infobytesec.com>
2015-03-25 19:09:15 +0100

    adjustment of play/pause icons

--- js/DOM.js
+++ js/DOM.js
@@ -9,9 +9,10 @@ function onTrackShow(evt){
     var trackLen = evt.tracks.length;
     for(var i = 0; i < trackLen; i++){
         var track = evt.tracks[i];
-        var itemContainer = $( "<div>", { class: "col-xs-12 col-sm-6 col-md-3 miniature placeholders"} );
-        $( '<img />', { src : track.artwork_url.replace('large','t200x200') } ).appendTo(itemContainer);
-        $( '<span>', { 'data-trackindex': trackCount + i, class : 'play glyphicon glyphicon-play' } ).appendTo(itemContainer);
+        var itemContainer = $( "<div>", { class: "col-xs-12 col-sm-6 col-md-3  placeholders"} );
+        var miniatureContainer = $( '<div>', { class : 'miniature' } ).appendTo(itemContainer);
+        $( '<img />', { src : track.artwork_url.replace('large','t200x200') } ).appendTo(miniatureContainer);
+        $( '<span>', { 'data-trackindex': trackCount + i, class : 'play glyphicon glyphicon-play' } ).appendTo(miniatureContainer);
         $('#content-target').append(itemContainer);
     }
     if(evt.finish){
